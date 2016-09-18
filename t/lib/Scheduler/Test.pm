2d5d77a3d11524914d82f9bfd60853b9cf3079d5
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2016-09-17 21:46:29 -0400

    minimal tests for System

--- t/lib/Scheduler/Test.pm
+++ t/lib/Scheduler/Test.pm
@@ -1,6 +1,10 @@
 package Scheduler::Test;
+use DDP;
 
-sub new { bless [ "Dummy Scheduler" ] => __PACKAGE__ }
+sub new { shift;
+          bless { name => "Dummy Scheduler", %{$_[0]} } => __PACKAGE__;
+        }
 sub schedule { }
+sub system { $_[0]{system} }
 
 1;
